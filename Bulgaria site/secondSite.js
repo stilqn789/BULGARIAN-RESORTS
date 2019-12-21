683336a3baf3019c436aa1217cfc6cf4cb453c96
Stilyan Tinkov Markovski <58570022+stilqn789@users.noreply.github.com>
2019-12-21 16:01:38 +0200

    Add files via upload

---
+++ Bulgaria site/secondSite.js
@@ -0,0 +1,16 @@
+function toggle(){
+    var dropdownM = document.getElementById("dropDownMenu" );
+    if (dropdownM.style.display == "none") {
+        dropdownM.style.display = "block";
+    } else {
+        dropdownM.style.display = "none";
+    }        
+}
+function toggleSecond(){
+    var dropdownMSecond = document.getElementById("dropDownMenuSecond" );
+    if (dropdownMSecond.style.display == "none") {
+        dropdownMSecond.style.display = "block";
+    } else {
+        dropdownMSecond.style.display = "none";
+    }        
+}
